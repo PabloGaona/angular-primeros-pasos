560477a1c7ba4ef15460cecdb6f805ea1ffe471c
Pablo Gaona <pgaona@ayesa.com>
2023-11-22 10:52:36 +0100

    Docs creado

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModules } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+
+
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // HeroComponent,
+    // ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModules,
+    DbzModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
